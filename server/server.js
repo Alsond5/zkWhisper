e09821868c130fad4eb5592cd09c9c99da302863
Alsond5 <ahmet_idris_0109@hotmail.com>
2024-01-29 13:36:24 +0300

    some necessary updates

--- server/server.js
+++ server/server.js
@@ -26,7 +26,32 @@ io.on("connection", (socket) => {
     socket.emit(`your room id: ${crypto.randomUUID()}`);
 
     socket.on("message", (message) => {
-        console.log(message);
+        if (typeof message === 'object') {
+            if (message.hasOwnProperty("op")) {
+                if (message["op"] === 2) {
+                    socket.to(message["rid"]).emit(message["proof"]);
+                }
+                else if (message["op" === 3]) {
+                    socket.to(message["rid"]).emit(message["content"]);
+                }
+            }
+        }
+    });
+
+    socket.on("create room", () => {
+        const rid = crypto.randomUUID();
+
+        socket.rooms.add(rid);
+        return rid;
+    });
+
+    socket.on("join room", (rid) => {
+        socket.rooms.add(rid);
+
+        socket.to(rid).emit("message", {
+            op: 3,
+            content: `${socket.id} joined to room!`
+        });
     });
 
     socket.on("disconnect", () => {
